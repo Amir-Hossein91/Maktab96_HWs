3cd71b6a9dc9ee8fa69b3f5a4aff8937cd136eab
Amir Hossein <amir_ahmadi9034@yahoo.com>
2023-06-28 20:26:01 +0430

    main methods of each class has been called in the main method of Main class in import.hw10.library module.(only for library importing practice)

--- import.hw10.library/src/main/java/Main.java
+++ import.hw10.library/src/main/java/Main.java
@@ -1,3 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        HW10Q1 question1 = new HW10Q1();
+        HW10Q2 question2 = new HW10Q2();
+        HW10Q3 question3 = new HW10Q3();
+        HashMapWhitOrder<String, Integer> map = new HashMapWhitOrder<>();
 
+        question1.main(args);
+
+        //question2.main(args);
+
+       // question3.main(args);
+
+       // map.main(args);
+    }
 }
