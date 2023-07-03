29fc5ff9ef347160004f78dc8961a8f7efe530f4
farzadafi <farzadafi40@gmail.com>
2023-07-03 13:32:22 +0330

    fix manageList and add a return statement instead of nested if

--- HW10/src/main/java/HW10Q3.java
+++ HW10/src/main/java/HW10Q3.java
@@ -7,28 +7,24 @@ public class HW10Q3 {
     public void run() {
         List<Integer> inputList = new ArrayList<>();
         Random random = new Random();
-
-        for(int i = 0; i < 10; i++){
-            inputList.add(random.nextInt(1,11));
+        for (int i = 0; i < 10; i++) {
+            inputList.add(random.nextInt(1, 11));
         }
-
         System.out.println(inputList);
         manageList(inputList);
         System.out.println(inputList);
     }
 
-    public static void manageList (List<Integer> list){
-        if(list != null){
-            if(list.size() % 2 != 0)
-                list.remove(list.size() - 1);
-
-            int size = list.size();
-
-            for(int i = size - 1; i >= 1; i -= 2){
-                if(list.get(i) > list.get(i-1)){
-                    list.remove(i);
-                    list.remove(i-1);
-                }
+    public static void manageList(List<Integer> list) {
+        if (list == null)
+            return;
+        if (list.size() % 2 != 0)
+            list.remove(list.size() - 1);
+        int size = list.size();
+        for (int i = size - 1; i >= 1; i -= 2) {
+            if (list.get(i) > list.get(i - 1)) {
+                list.remove(i);
+                list.remove(i - 1);
             }
         }
     }
