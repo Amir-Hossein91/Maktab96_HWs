0ea687fed2f22de5f8a84e4ee1656313bac1889b
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-07-17 01:39:35 +0430

    question 2 completed

---
+++ HW12/src/main/java/cw12/question2/HW12Part1.java
@@ -0,0 +1,18 @@
+package cw12.question2;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HW12Part1 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Amir", "Hossein", "Ali", "Amin", "Eli", "Mahtab", "Maryam", "Omid");
+        Map<Integer, List<String>> names = words.stream().collect(Collectors.groupingBy(String::length));
+        System.out.println(names.values());
+        System.out.println(names);
+    }
+
+
+}
