f7350009611fa6024d0489c75dfa20d58b1748f5
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-07-27 02:26:01 +0430

    HW13: tasks B4 and B5 completed

--- HW13/src/main/java/question1/Main.java
+++ HW13/src/main/java/question1/Main.java
@@ -4,8 +4,7 @@ package question1;
 import question1.mockdata.MockData;
 import question1.model.Person;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,15 +13,26 @@ public class Main {
 //        for (Person p : filterAgeLessThanFifty()) {
 //            System.out.println(p.getAge());
 //        }
-//
+
 //        //B-2:
 //        for (Person p : sortByUsername()) {
 //            System.out.println(p.getUsername());
 //        }
 
-        //B-3
-        for (Person p : sortByAgeAndLastname()) {
-            System.out.println(p.getAge() + "\t" + p.getLastName());
+//        //B-3
+//        for (Person p : sortByAgeAndLastname()) {
+//            System.out.println(p.getAge() + "\t" + p.getLastName());
+//        }
+
+//        //B-4
+//        for (String s: mapToIpv4()){
+//            System.out.println(s);
+//        }
+
+        //B-5
+        System.out.println(fetchHundredPeople().size());
+        for(Map.Entry<String, Person> entry :fetchHundredPeople().entrySet()){
+            System.out.println(entry);
         }
 
 
@@ -43,4 +53,22 @@ public class Main {
         return MockData.getPeople().stream()
                 .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getLastName)).toList();
     }
+
+    public static List<String> mapToIpv4(){
+        return MockData.getPeople().stream().map(Person::getIpv4).toList();
+    }
+
+    public static Map<String, Person> fetchHundredPeople(){
+        Map<String, Person> result = new HashMap<>();
+       MockData.getPeople().stream()
+                .sorted(Comparator.comparing(Person::getLastName))
+                .filter(person -> person.getGender().equalsIgnoreCase("female") && person.getAge() > 40)
+                .sorted(Comparator.comparing(Person::getFirstName))
+                .dropWhile(person -> person.getFirstName().startsWith("A"))
+                .skip(5)
+                .limit(100)
+                .forEach(person -> result.put(person.getFirstName() + " " + person.getLastName(),person));
+        return result;
+    }
+
 }
