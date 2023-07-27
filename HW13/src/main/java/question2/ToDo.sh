c4e51b04db2b1e95b93f3ea7970c1b2b9b18348d
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-07-28 00:47:10 +0430

    HW13: more process on qustion2

--- HW13/src/main/java/question2/ToDo.sh
+++ HW13/src/main/java/question2/ToDo.sh
@@ -1,47 +1,33 @@
-#echo -n "Enter the name of a country: "
-#read COUNTRY
-#
-#echo -n "The official language of $COUNTRY is "
-#
-#case $COUNTRY in
-#
-#  Lithuania)
-#    echo -n "Lithuanian"
-#    ;;
-#
-#  Romania | Moldova)
-#    echo -n "Romanian"
-#    ;;
-#
-#  Italy | "San Marino" | Switzerland | "Vatican City")
-#    echo -n "Italian"
-#    ;;
-#
-#  *)
-#    echo -n "unknown"
-#    ;;
-#esac
 showUndoneMenu() {
-  while ( true ) do
-  clear
-  undoneMenu=("Show Undone List" "Search in Undone List" "Back")
-  for (( i = 0; i < ${#undoneMenu[@]}; i++ )); do
-        echo "$((i+1))) " ${undoneMenu[i]}
+  while (true); do
+    clear
+    undoneMenu=("Show ToDo List" "Mark a New Task As Done" "Add a Task" "Remove a Task" "Search in ToDo List" "Back")
+    for ((i = 0; i < ${#undoneMenu[@]}; i++)); do
+      echo "$((i + 1))) " ${undoneMenu[i]}
     done
-  read -p "Choose an option: " choice2
+    read -p "Choose an option: " choice2
     case $choice2 in
     1)
       cd tasks || return
-            cat -n < Undone.txt
-            cd ..
-            echo
-            read -p "press enter to go back"
+      cat -n < Undone.txt
+      cd ..
+      echo
+      read -p "press enter to go back"
       ;;
     2)
       echo "you want to search in the list"
       read -p "press enter to go back"
       ;;
     3)
+      read -p "press enter to go back"
+      ;;
+    4)
+      read -p "press enter to go back"
+      ;;
+    5)
+      read -p "press enter to go back"
+      ;;
+    6)
       break
       ;;
     esac
@@ -55,35 +41,31 @@ cd ..
 while (true); do
   clear
   mainList=("Undone Tasks" "Done Tasks" "Removed Tasks" "Exit")
-  for (( i = 0; i < ${#mainList[@]}; i++ )); do
-      echo "$((i+1))) " ${mainList[i]}
+  for ((i = 0; i < ${#mainList[@]}; i++)); do
+    echo "$((i + 1))) " ${mainList[i]}
   done
-    read -p "Choose an option: " choice
-    case $choice in
-    1)
-      showUndoneMenu
-      ;;
-    2)
-      cd tasks || return
-      cat -n < Done.txt
-      cd ..
-      echo
-      read -p "press enter to go back"
-      ;;
-    3)
-      echo "you want to see the removed tasks"
-      read -p "press enter to go back"
-      ;;
+  read -p "Choose an option: " choice
+  case $choice in
+  1)
+    showUndoneMenu
+    ;;
+  2)
+    cat -b < tasks/Done.txt
+    echo
+    read -p "press enter to go back"
+    ;;
+  3)
+    echo "you want to see the removed tasks"
+    read -p "press enter to go back"
+    ;;
 
-    4)
-      exit
-      ;;
-    *)
-      echo "Wrong Entry!"
-      ;;
-    esac
-
+  4)
+    clear
+    exit
+    ;;
+  *)
+    echo "Wrong Entry!"
+    ;;
+  esac
 
 done
-
-
