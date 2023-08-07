311be5e278b8c700c1001bf028c6d162c50dcb67
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-07 20:24:54 +0430

    HW14: question2: application context altered

--- HW14/src/main/java/qustion2/utility/ApplicationContext.java
+++ HW14/src/main/java/qustion2/utility/ApplicationContext.java
@@ -1,8 +1,11 @@
 package qustion2.utility;
 
-import qustion2.repository.PersonRepository;
-import qustion2.repository.StudentRepository;
-import qustion2.service.PersonService;
+import qustion2.repository.impl.PersonRepositoryImpl;
+import qustion2.repository.impl.StudentRepositoryImpl;
+import qustion2.repository.impl.TeacherRepositoryImpl;
+import qustion2.service.impl.PersonServiceImpl;
+import qustion2.service.impl.StudentServiceImpl;
+import qustion2.service.impl.TeacherServiceImpl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,11 +14,14 @@ import javax.persistence.Persistence;
 public class ApplicationContext {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hw14");
     private static final EntityManager em = emf.createEntityManager();
-    private static final PersonRepository pr = new PersonRepository(em);
-    private static final StudentRepository sr = new StudentRepository();
+    private static final PersonRepositoryImpl pr = new PersonRepositoryImpl(em);
+    private static final StudentRepositoryImpl sr = new StudentRepositoryImpl(em);
+    private static final TeacherRepositoryImpl tr = new TeacherRepositoryImpl(em);
 
 
-    public static final PersonService personService = new PersonService(pr);
+    public static final PersonServiceImpl personService = new PersonServiceImpl(pr);
+    public static final StudentServiceImpl studentService = new StudentServiceImpl(sr);
+    public static final TeacherServiceImpl teacherService = new TeacherServiceImpl(tr);
 
 
 }
