8b56086b9c5ee18ad5e35298ffeedbcf1ac50a9f
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-09 22:57:55 +0430

    HW14: question2: connection and validation packages created

---
+++ HW14/src/main/java/question2/connection/Connection.java
@@ -0,0 +1,12 @@
+package question2.connection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connection {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hw14");
+    public static final EntityManager entityManager = emf.createEntityManager();
+
+
+}
