ad64f53ccd69539f46d6dba7d70513eaf6529848
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-09 23:00:12 +0430

    HW14: question2: altered ApplicationContext

--- HW14/src/main/java/question2/utility/ApplicationContext.java
+++ HW14/src/main/java/question2/utility/ApplicationContext.java
@@ -1,19 +1,22 @@
 package question2.utility;
 
+import jakarta.validation.Validator;
+import question2.connection.Connection;
 import question2.repository.impl.PersonRepositoryImpl;
 import question2.repository.impl.StudentRepositoryImpl;
 import question2.repository.impl.TeacherRepositoryImpl;
 import question2.service.impl.PersonServiceImpl;
 import question2.service.impl.StudentServiceImpl;
 import question2.service.impl.TeacherServiceImpl;
+import question2.validation.EntityValidator;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class ApplicationContext {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hw14");
-    private static final EntityManager em = emf.createEntityManager();
+    private static final EntityManager em = Connection.entityManager;
+
     private static final PersonRepositoryImpl pr = new PersonRepositoryImpl(em);
     private static final StudentRepositoryImpl sr = new StudentRepositoryImpl(em);
     private static final TeacherRepositoryImpl tr = new TeacherRepositoryImpl(em);
@@ -23,5 +26,5 @@ public class ApplicationContext {
     public static final StudentServiceImpl studentService = new StudentServiceImpl(sr);
     public static final TeacherServiceImpl teacherService = new TeacherServiceImpl(tr);
 
-
+    public static final Validator validator = EntityValidator.validator;
 }
