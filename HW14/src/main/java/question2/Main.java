605b00cb708e9574423046c47bc3f55e0354fec5
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-10 05:22:31 +0430

    HW14: question2: created some method calls to run the program

--- HW14/src/main/java/question2/Main.java
+++ HW14/src/main/java/question2/Main.java
@@ -1,6 +1,12 @@
 package question2;
 
 
+import question2.entity.Person;
+import question2.entity.Student;
+import question2.entity.Teacher;
+import question2.entity.enums.Degree;
+import question2.entity.enums.FacultyLevel;
+import question2.entity.enums.StudyField;
 import question2.service.impl.PersonServiceImpl;
 import question2.service.impl.StudentServiceImpl;
 import question2.service.impl.TeacherServiceImpl;
@@ -13,26 +19,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(personService.signUp("alireza", "gharibi"));
-//
-//        System.out.println(personService.contains(new Person(6,"alireza","gharibi",null)));
+//        System.out.println(personService.signUp("mohammad", "heidari"));
 
-//        personService.delete(new Person(3,"amin","dehbandi",null));
+//        System.out.println(teacherService.signUp("amin","kabiri","36261", Degree.PHD, FacultyLevel.MASTER));
+
+//        System.out.println(studentService.signUp("omid","ghanbari","36254", StudyField.IT));
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//        System.out.println(personService.contains(new Person(1,"mohammad","heidari",null)));
+
+//        System.out.println(teacherService.contains(new Teacher(2,"amin","kabiri",null,"36261",Degree.PHD,FacultyLevel.MASTER,0)));
+
+//        System.out.println(studentService.contains(new Student(3,"ali","ghanbari",null,"36254", StudyField.IT,2010)));
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //        personService.loadAll().forEach(System.out::println);
-
-//        personService.update(new Person(5,"amin", "dehbandi", null));
-
-//        System.out.println(teacherService.signUp("ali","kabiri","362547"));
 
 //        teacherService.loadAll().forEach(System.out::println);
 
 //        studentService.loadAll().forEach(System.out::println);
 
-//        System.out.println(teacherService.contains(new Teacher(7,"ali","kabiri",null,"362547",null,null,0)));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//        personService.delete(new Person(3,"amin","dehbandi",null));
+
+//        personService.update(new Person(5,"amin", "dehbandi", null));
 
 
-//        System.out.println(studentService.signUp("ali","ghanbari","362547"));
+
+
 
 
     }
