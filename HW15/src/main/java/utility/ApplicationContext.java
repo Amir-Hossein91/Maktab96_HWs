ddf86b332d388b148f74cd6c04d4d0133df34b3a
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-16 16:49:05 +0430

    HW15: created ApplicationContext

--- HW15/src/main/java/utility/ApplicationContext.java
+++ HW15/src/main/java/utility/ApplicationContext.java
@@ -1,4 +1,41 @@
 package utility;
 
+import connection.Connection;
+import repository.*;
+import service.impl.*;
+
+import javax.persistence.EntityManager;
+
 public class ApplicationContext {
+    private static final CourseRepositroyImpl courseRepositroy;
+    private static final EmployeeRepositoryImpl employeeRepository;
+    private static final PersonRepositoryImpl personRepository;
+    private static final SalaryReportRepositoryImpl salaryReportRepository;
+    private static final ScoreRepositoryImpl scoreRepository;
+    private static final StudentRepositoryImpl studentRepository;
+    private static final TeacherRepositoryImpl teacherRepository;
+    public static final CourseServiceImpl courseService;
+    public static final EmployeeServiceImpl employeeService;
+    public static final PersonServiceImpl personService;
+    public static final SalaryReportServiceImpl salaryReportService;
+    public static final ScoreServiceImpl scoreService;
+    public static final StudentServiceImpl studentService;
+    public static final TeacherServiceImpl teacherService;
+
+    static {
+        courseRepositroy = new CourseRepositroyImpl();
+        employeeRepository = new EmployeeRepositoryImpl();
+        personRepository = new PersonRepositoryImpl();
+        salaryReportRepository = new SalaryReportRepositoryImpl();
+        scoreRepository = new ScoreRepositoryImpl();
+        studentRepository = new StudentRepositoryImpl();
+        teacherRepository = new TeacherRepositoryImpl();
+        courseService = new CourseServiceImpl(courseRepositroy);
+        employeeService = new EmployeeServiceImpl(employeeRepository);
+        personService = new PersonServiceImpl(personRepository);
+        salaryReportService = new SalaryReportServiceImpl(salaryReportRepository);
+        scoreService = new ScoreServiceImpl(scoreRepository);
+        studentService = new StudentServiceImpl(studentRepository);
+        teacherService = new TeacherServiceImpl(teacherRepository);
+    }
 }
