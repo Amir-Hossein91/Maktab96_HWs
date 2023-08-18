a8727c99ed22c4a39753cd13255b9fbdb6bd3e4b
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-18 17:37:58 +0430

    HW15: more constant massages

--- HW15/src/main/java/utility/Constants.java
+++ HW15/src/main/java/utility/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final String SCORE_SAVE_EXCEPTION = "Could not save the score!";
     public static final String COURSE_SAVE_EXCEPTION = "Could not save the course!";
     public static final String EMPLOYEE_SAVE_EXCEPTION = "Could not save the employee!";
+    public static final String SALARY_REPORT_SAVE_EXCEPTION = "Could not save the salary report!";
+    public static final String PERSON_SAVE_EXCEPTION = "Could not save user!";
+    public static final String STUDENT_SAVE_EXCEPTION = "Could not save the student!";
+    public static final String TEACHER_SAVE_EXCEPTION = "Could not save the teacher!";
 }
