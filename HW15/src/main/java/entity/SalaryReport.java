1ed8417d6f3494b5b92daa3fe5f1bfdb8c8f1dda
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-20 14:02:13 +0430

    HW15: validations in salary report

--- HW15/src/main/java/entity/SalaryReport.java
+++ HW15/src/main/java/entity/SalaryReport.java
@@ -2,10 +2,13 @@ package entity;
 
 import entity.baseEntity.BaseEntity;
 import lombok.*;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,12 +19,16 @@ import java.util.Date;
 public class SalaryReport extends BaseEntity {
     @OneToOne
     private UniversityStaff owner;
+    @Temporal(value = TemporalType.DATE)
+    @Column(name = "report_date")
     private Date reportDate;
+    @NotNull(message = "Specify the salary amount")
     private long salaryAmount;
 
     public SalaryReport(UniversityStaff owner) {
        this.owner = owner;
-        reportDate = new Date();
-        salaryAmount = owner.getTotalSalary();
+       Calendar calendar = new GregorianCalendar();
+       reportDate =calendar.getTime();
+       salaryAmount = owner.getTotalSalary();
     }
 }
