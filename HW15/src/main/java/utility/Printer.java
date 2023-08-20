c165993cf7eecbede9a895b58d3c25af9ea52a38
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-20 23:11:25 +0430

    HW15: added some methods to Printer class

--- HW15/src/main/java/utility/Printer.java
+++ HW15/src/main/java/utility/Printer.java
@@ -1,4 +1,50 @@
 package utility;
 
+import java.util.List;
+
 public class Printer {
+
+    public void printMenu(String[] menu){
+        String titleBorder = "====================================";
+        String endOfMenu = "------------------------------------";
+        System.out.println(titleBorder);
+        int shift = (titleBorder.length() - menu[0].length())/2;
+        for (int i =1; i<= shift; i++)
+            System.out.print(" ");
+        System.out.println(menu[0]);
+        System.out.println(titleBorder);
+        for(int i=1; i<menu.length; i++)
+            System.out.println(i + ") " + menu[i]);
+        System.out.println(endOfMenu);
+        askToSelect();
+    }
+
+    public void askToSelect(){
+        System.out.print("Select--> ");
+    }
+
+    public void getInput(String msg){
+        System.out.print("*) " + msg + " --> ");
+    }
+
+    public void printMessage(String msg){
+        System.out.println("******" + msg + "******");
+    }
+
+    public void printError(String msg){
+        System.out.println(">>>> " + msg + " <<<<");
+    }
+
+    public void printResult(String title, List<String> result){
+        String titleBorder = "************************************";
+        String endOfResult = "------------------------------------";
+        System.out.println(titleBorder);
+        int shift = (titleBorder.length() - title.length())/2;
+        for (int i =1; i<= shift; i++)
+            System.out.print(" ");
+        System.out.println(title);
+        System.out.println(titleBorder);
+        result.forEach(System.out::println);
+        System.out.println(endOfResult);
+    }
 }
