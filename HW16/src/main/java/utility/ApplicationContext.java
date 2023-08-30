0343c5fcb19915b28b7c467586660742404e4823
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-31 01:52:32 +0430

    HW16: ApplicationContext and Constants and EntityValidator classes just created but not configured

---
+++ HW16/src/main/java/utility/ApplicationContext.java
@@ -0,0 +1,8 @@
+package utility;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class ApplicationContext {
+    public static final Date currentDate = new GregorianCalendar(2023,02,19).getTime();
+}
---
+++ HW16/src/main/java/utility/Constants.java
@@ -0,0 +1,8 @@
+package utility;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Constants {
+
+}
