f3b138ce1cee08a416b1cdaf911699566f85f4ee
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-08-31 14:23:07 +0430

    HW16: added currentDate and printer to applicationContext

--- HW16/src/main/java/utility/ApplicationContext.java
+++ HW16/src/main/java/utility/ApplicationContext.java
@@ -4,5 +4,12 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class ApplicationContext {
-    public static final Date currentDate = new GregorianCalendar(2023,02,19).getTime();
+    public static Date currentDate ;
+
+    public static Printer printer;
+
+    static{
+        currentDate = new GregorianCalendar(2023,02,19).getTime();
+        printer = new Printer();
+    }
 }
