686ded1f6738332161e0bb0f98ee8f22f59238b8
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-04 16:59:29 +0430

    HW16: added some validation to BankAccount

--- HW16/src/main/java/entity/BankAccount.java
+++ HW16/src/main/java/entity/BankAccount.java
@@ -4,10 +4,11 @@ import entity.base.BaseEntity;
 import entity.enums.Bank;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.Range;
+import utility.Constants;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 
 @SuperBuilder
 @Getter
@@ -19,12 +20,18 @@ import javax.persistence.SequenceGenerator;
 @Entity
 @SequenceGenerator(name = "idGenerator" , sequenceName = "bankAccount_Sequence")
 public class BankAccount extends BaseEntity {
+    @Enumerated(value = EnumType.STRING)
     private Bank bank;
+    @Pattern(regexp = "^[0-9]{16}$", message = Constants.INVALID_CARD_NUMBER_EXCEPTION)
     private String cardNumber;
+    @Pattern(regexp = "^[0-9]{3,4}$", message = Constants.INVALID_CVV2_NUMBER_EXCEPTION)
     private String cvv2;
-    private String expirationMonth;
-    private String expirationYear;
-    @OneToOne
+    @Range(min = 1, max = 12, message = Constants.INVALID_EXPIRATION_MONTH_EXCEPTION)
+    private int expirationMonth;
+    @Pattern(regexp = "^[1-2][0-9]{3}$" , message = Constants.INVALID_EXPIRATION_YEAR_EXCEPTION)
+    private int expirationYear;
+    @OneToOne(mappedBy = "bankAccount")
     private Student owner;
+    @Range(min = 0)
     private Long balance;
 }
