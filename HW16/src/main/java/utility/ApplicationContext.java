764e49c9072ea6515af01366da93d7a4504bc6fb
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-04 16:58:21 +0430

    HW16: altered ApplicationContext

--- HW16/src/main/java/utility/ApplicationContext.java
+++ HW16/src/main/java/utility/ApplicationContext.java
@@ -8,15 +8,15 @@ import repository.BankAccountRepositoryImpl;
 import repository.DebtRepositoryImpl;
 import repository.LoanRepositoryImpl;
 import repository.StudentRepositoryImpl;
-import service.BankAccountServiceImpl;
-import service.DebtServiceImpl;
-import service.LoanServiceImpl;
-import service.StudentServiceImpl;
+import service.impl.BankAccountServiceImpl;
+import service.impl.DebtServiceImpl;
+import service.impl.LoanServiceImpl;
+import service.impl.StudentServiceImpl;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.*;
 
 public class ApplicationContext {
+    public final static GregorianCalendar gregorianCalendar;
     public final static Date currentDate ;
     public final static Printer printer;
     private final static BankAccountRepositoryImpl bankAccountRepository;
@@ -27,9 +27,11 @@ public class ApplicationContext {
     public final static StudentServiceImpl studentService;
     public final static LoanServiceImpl loanService;
     public final static DebtServiceImpl debtService;
+    public final static Map<Integer,Character[]> passwordCharactersMap;
 
     static{
-        currentDate = new GregorianCalendar(2023,02,19).getTime();
+        gregorianCalendar = new GregorianCalendar(2020,Calendar.APRIL,19);
+        currentDate = gregorianCalendar.getTime();
         printer = new Printer();
         bankAccountRepository = new BankAccountRepositoryImpl(BankAccount.class);
         studentRepository = new StudentRepositoryImpl(Student.class);
@@ -39,5 +41,11 @@ public class ApplicationContext {
         studentService = new StudentServiceImpl(studentRepository);
         loanService = new LoanServiceImpl(loanRepository);
         debtService = new DebtServiceImpl(debtRepository);
+        passwordCharactersMap = new HashMap<>(){{
+           put(1,Constants.UPPER_CASE_CHARACTERS);
+           put(2,Constants.LOWER_CASE_CHARACTERS);
+           put(3,Constants.DIGIT_CHARACTERS);
+           put(4,Constants.SPECIAL_CHARACTERS);
+        }};
     }
 }
