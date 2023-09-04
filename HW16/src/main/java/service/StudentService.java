7ba6992c818f1fc86f322c704d2c59897043f5ae
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-04 21:42:23 +0430

    HW16: improved StudentServiceImpl and tried to obey clean code rules

--- HW16/src/main/java/service/StudentService.java
+++ HW16/src/main/java/service/StudentService.java
@@ -3,10 +3,13 @@ package service;
 import basics.service.BaseService;
 import entity.Student;
 import exceptions.InvalidDateException;
+import exceptions.NotProperTimeException;
 
 public interface StudentService extends BaseService<Student> {
     Student validateAndSetInformation(Student student) throws InvalidDateException;
     Student setUsernameAndPassword(Student student);
     void showUsernameAndPassword(Student student);
-    void checkUsernameAndPassword();
+    Student checkUsernameAndPassword(String username, String password);
+    boolean canRegister(Student student) throws NotProperTimeException;
+    boolean canRepay(Student student) throws NotProperTimeException;
 }
--- HW16/src/main/java/service/impl/StudentServiceImpl.java
+++ HW16/src/main/java/service/impl/StudentServiceImpl.java
@@ -5,7 +5,8 @@ import entity.BankAccount;
 import entity.Student;
 import entity.enums.*;
 import exceptions.InvalidDateException;
-import repository.StudentRepositoryImpl;
+import exceptions.NotProperTimeException;
+import repository.impl.StudentRepositoryImpl;
 import service.StudentService;
 import utility.ApplicationContext;
 import utility.Constants;
@@ -27,78 +28,34 @@ public class StudentServiceImpl extends BaseServiceImpl<StudentRepositoryImpl, S
     public Student validateAndSetInformation(Student student) throws InvalidDateException {
         try{
             printer.printMessage("Pleas fill the required fields:");
-            printer.getInput("First name");
-            student.setFirstname(input.nextLine());
-            printer.getInput("Last name");
-            student.setLastname(input.nextLine());
-            printer.getInput("Father name");
-            student.setFathername(input.nextLine());
-            printer.getInput("Mother name");
-            student.setMothername(input.nextLine());
-            printer.getInput("Identity serial number");
-            student.setIdentityCode(input.nextLine());
-            printer.getInput("National code");
-            student.setNationalCode(input.nextLine());
-            do{
-                student.setBirthDate(setDate());
-            } while (student.getBirthDate()==null);
-            printer.printMessage("Select your province from list bellow");
-            printer.printListWithSelect(Arrays.stream(Province.values()).map(Object::toString).toList());
-            student.setProvince(Province.values()[input.nextInt()-1]);
-            printer.printMessage("Marital Status");
-            printer.printListWithSelect(Arrays.stream(MaritalStatus.values()).map(Objects::toString).toList());
-            int choice = input.nextInt();
-            input.nextLine();
-            student.setMarried(MaritalStatus.values()[choice - 1].name().equals("MARRIED"));
-            if(student.isMarried()){
-                printer.getInput("Spouse's first name");
-                student.setSpouseFirstName(input.nextLine());
-                printer.getInput("Spouse's last name");
-                student.setSpouseLastName(input.nextLine());
-                printer.getInput("Spouse's national code");
-                student.setSpouseNationalCode(input.next());
-            }
-            printer.printMessage("University type");
-            printer.printListWithSelect(Arrays.stream(UniversityType.values()).map(Objects::toString).toList());
-            student.setUniversityType(UniversityType.values()[input.nextInt()-1]);
-            printer.getInput("Entrance year");
-            int entranceYear = input.nextInt();
-            input.nextLine();
-            if(entranceYear >= (student.getBirthYear() + 15))
-                student.setEntranceYear(entranceYear);
-            else
-                throw new InvalidDateException(Constants.INVALID_AGE_EXCEPTION);
-            printer.getInput("Student code");
-            student.setStudentNumber(input.next());
-            input.nextLine();
-            printer.printMessage("Acceptance type");
-            printer.printListWithSelect(Arrays.stream(AcceptanceType.values()).map(Objects::toString).toList());
-            student.setAcceptanceType(AcceptanceType.values()[input.nextInt()-1]);
-            input.nextLine();
-            printer.printMessage("Academic degree");
-            printer.printListWithSelect(Arrays.stream(AcademicGrade.values()).map(Objects::toString).toList());
-            student.setAcademicGrade(AcademicGrade.values()[input.nextInt()-1]);
-            input.nextLine();
-            printer.printMessage("Residence status");
-            printer.printListWithSelect(Arrays.stream(ResidenceStatus.values()).map(Object::toString).toList());
-            student.setInDorm(input.nextInt() == ResidenceStatus.DORM.ordinal()+1);
-            input.nextLine();
-            if(!student.isInDorm()){
-                printer.getInput("House rent contract number");
-                student.setHouseContractNumber(input.next());
-                input.nextLine();
-                printer.getInput("House address");
-                student.setHouseAddress(input.nextLine());
-            }
-            do {
-                student.setBankAccount(bankAccountService.validateAndSetInformation(new BankAccount()));
-            } while (student.getBankAccount() == null);
+            getFirstName(student);
+            getLastName(student);
+            getFatherName(student);
+            getMotherName(student);
+            getIdentityNumber(student);
+            getNationalCode(student);
+            getBirthDate(student);
+            getProvince(student);
+            getMaritalStatus( student);
+            if(student.isMarried())
+                getSpouseInformation(student);
+            getUniversityType(student);
+            getEntranceYear(student);
+            getStudentCode(student);
+            getAcceptanceType(student);
+            getAcademicDegree(student);
+            getResidenceStatus(student);
+            if(!student.isInDorm())
+                getAddress(student);
+            getBankAccount(student);
 
             student.setGraduateYear(student.getEntranceYear()+student.getAcademicGrade().getGraduateDuration());
+
             if(isValid(student))
                 return student;
             else
                 return null;
+
         }catch (Exception e){
             student = null;
             throw e;
@@ -148,8 +105,29 @@ public class StudentServiceImpl extends BaseServiceImpl<StudentRepositoryImpl, S
     }
 
     @Override
-    public void checkUsernameAndPassword() {
+    public Student checkUsernameAndPassword(String username, String password) {
+        try{
+            return repository.checkUsernameAndPassword(username,password);
+        } catch (Exception e){
+            printer.printError(Constants.INCORRECT_USERNAME_OR_PASSWORD);
+            return null;
+        }
+    }
 
+    @Override
+    public boolean canRegister(Student student) throws NotProperTimeException {
+        boolean result = ApplicationContext.gregorianCalendar.get(Calendar.YEAR) <= student.getGraduateYear();
+        if(!result)
+            throw new NotProperTimeException(Constants.LOAN_REGISTER_TIME_OUT);
+        return result;
+    }
+
+    @Override
+    public boolean canRepay(Student student) throws NotProperTimeException {
+        boolean result = ApplicationContext.gregorianCalendar.get(Calendar.YEAR) > student.getGraduateYear();
+        if(!result)
+            throw new NotProperTimeException(Constants.REPAY_TIME_OUT);
+        return result;
     }
 
     public Date setDate() {
@@ -180,6 +158,119 @@ public class StudentServiceImpl extends BaseServiceImpl<StudentRepositoryImpl, S
         }
     }
 
+    public String getUsername(){
+        printer.getInput("Username");
+        String result = input.next();
+        input.nextLine();
+        return result;
+    }
+
+    public String getPassword(){
+        printer.getInput("Password");
+        String result = input.next();
+        input.nextLine();
+        return result;
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void getFirstName(Student student){
+        printer.getInput("First name");
+        student.setFirstname(input.nextLine());
+    }
+    private void getLastName(Student student) {
+        printer.getInput("Last name");
+        student.setLastname(input.nextLine());
+    }
+    private void getFatherName(Student student) {
+        printer.getInput("Father name");
+        student.setFathername(input.nextLine());
+    }
+    private void getMotherName(Student student) {
+        printer.getInput("Mother name");
+        student.setMothername(input.nextLine());
+    }
+    private void getIdentityNumber(Student student) {
+        printer.getInput("Identity serial number");
+        student.setIdentityCode(input.nextLine());
+    }
+    private void getNationalCode(Student student) {
+        printer.getInput("National code");
+        student.setNationalCode(input.nextLine());
+    }
+    private void getBirthDate(Student student) {
+        do {
+            student.setBirthDate(setDate());
+        } while (student.getBirthDate() == null);
+    }
+    private void getProvince(Student student) {
+        printer.printMessage("Select your province from list bellow");
+        printer.printListWithSelect(Arrays.stream(Province.values()).map(Object::toString).toList());
+        student.setProvince(Province.values()[input.nextInt() - 1]);
+    }
+    private void getMaritalStatus(Student student) {
+        printer.printMessage("Marital Status");
+        printer.printListWithSelect(Arrays.stream(MaritalStatus.values()).map(Objects::toString).toList());
+        int choice = input.nextInt();
+        input.nextLine();
+        student.setMarried(MaritalStatus.values()[choice - 1].name().equals("MARRIED"));
+    }
+    private void getSpouseInformation(Student student) {
+        printer.getInput("Spouse's first name");
+        student.setSpouseFirstName(input.nextLine());
+        printer.getInput("Spouse's last name");
+        student.setSpouseLastName(input.nextLine());
+        printer.getInput("Spouse's national code");
+        student.setSpouseNationalCode(input.next());
+    }
+    void getUniversityType(Student student) {
+        printer.printMessage("University type");
+        printer.printListWithSelect(Arrays.stream(UniversityType.values()).map(Objects::toString).toList());
+        student.setUniversityType(UniversityType.values()[input.nextInt() - 1]);
+    }
+    private void getEntranceYear(Student student) throws InvalidDateException {
+        printer.getInput("Entrance year");
+        int entranceYear = input.nextInt();
+        input.nextLine();
+        if (entranceYear >= (student.getBirthYear() + 15))
+            student.setEntranceYear(entranceYear);
+        else
+            throw new InvalidDateException(Constants.INVALID_AGE_EXCEPTION);
+    }
+    private void getStudentCode(Student student) {
+        printer.getInput("Student code");
+        student.setStudentNumber(input.next());
+        input.nextLine();
+    }
+    private void getAcceptanceType(Student student) {
+        printer.printMessage("Acceptance type");
+        printer.printListWithSelect(Arrays.stream(AcceptanceType.values()).map(Objects::toString).toList());
+        student.setAcceptanceType(AcceptanceType.values()[input.nextInt() - 1]);
+        input.nextLine();
+    }
+    private void getAcademicDegree(Student student) {
+        printer.printMessage("Academic degree");
+        printer.printListWithSelect(Arrays.stream(AcademicGrade.values()).map(Objects::toString).toList());
+        student.setAcademicGrade(AcademicGrade.values()[input.nextInt() - 1]);
+        input.nextLine();
+    }
+    private void getResidenceStatus(Student student) {
+        printer.printMessage("Residence status");
+        printer.printListWithSelect(Arrays.stream(ResidenceStatus.values()).map(Object::toString).toList());
+        student.setInDorm(input.nextInt() == ResidenceStatus.DORM.ordinal() + 1);
+        input.nextLine();
+    }
+    private void getAddress(Student student) {
+        printer.getInput("House rent contract number");
+        student.setHouseContractNumber(input.next());
+        input.nextLine();
+        printer.getInput("House address");
+        student.setHouseAddress(input.nextLine());
+    }
+    private void getBankAccount(Student student) {
+        do {
+            student.setBankAccount(bankAccountService.validateAndSetInformation(new BankAccount()));
+        } while (student.getBankAccount() == null);
+    }
 
 
 }
