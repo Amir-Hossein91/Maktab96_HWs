1b185c5fab2b5163c3cd0aa05cea59de7146a2f1
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-07 02:58:58 +0430

    HW16: added a two arguments constructo for Loan and a method to set the amount of each loan

--- HW16/src/main/java/entity/Loan.java
+++ HW16/src/main/java/entity/Loan.java
@@ -1,8 +1,9 @@
 package entity;
 
 import entity.base.BaseEntity;
-import entity.enums.Availability;
+import entity.enums.AcademicGrade;
 import entity.enums.LoanType;
+import entity.enums.Province;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -14,7 +15,6 @@ import java.util.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @SequenceGenerator(name = "idGenerator" , sequenceName = "loan_Sequence")
@@ -22,9 +22,55 @@ public class Loan extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     private LoanType loanType;
     private Long amount;
-    @Enumerated(value = EnumType.STRING)
-    private Availability availability;
     @ManyToOne
-    private Student loanee;
+    private Student borrower;
     private Date registrationDate;
+
+    public Loan (LoanType loanType, Student borrower){
+        this.loanType = loanType;
+        this.borrower = borrower;
+        setLoanAmount(loanType,borrower);
+    }
+
+    private void setLoanAmount(LoanType loanType, Student borrower){
+        switch (loanType){
+            case EDUCATIONAL -> {
+                AcademicGrade academicGrade = borrower.getAcademicGrade();
+                if(academicGrade == AcademicGrade.ASSOCIATE || academicGrade == AcademicGrade.INTERRUPTED_BACHELOR
+                        || academicGrade == AcademicGrade.UNINTERRUPTED_BACHELOR)
+                    amount = 1_900_000L;
+                else if(academicGrade == AcademicGrade.INTERRUPTED_DOCTORATE)
+                    amount = 2_600_000L;
+                else
+                    amount = 2_250_000L;
+            }
+            case TUITION -> {
+                AcademicGrade academicGrade = borrower.getAcademicGrade();
+                if(academicGrade == AcademicGrade.ASSOCIATE || academicGrade == AcademicGrade.INTERRUPTED_BACHELOR
+                        || academicGrade == AcademicGrade.UNINTERRUPTED_BACHELOR)
+                    amount = 1_300_000L;
+                else if(academicGrade == AcademicGrade.INTERRUPTED_DOCTORATE)
+                    amount = 6_500_000L;
+                else
+                    amount = 2_600_000L;
+            }
+            case HOUSE_RENT -> {
+                Province province = borrower.getProvince();
+                if (province == Province.TEHRAN)
+                    amount = 32_000_000L;
+                else if(province.isMetropolis())
+                    amount = 26_000_000L;
+                else
+                    amount = 19_500_000L;
+            }
+            default -> {}
+        }
+    }
+
+    public String toString() {
+        return "Loan(loanType=" + this.getLoanType() +
+                ", amount=" + this.getAmount() +
+                ", borrowerId=" + this.getBorrower().getId() +
+                ", registrationDate=" + this.getRegistrationDate() + ")";
+    }
 }
