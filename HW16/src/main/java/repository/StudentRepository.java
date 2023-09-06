1a80c916c7e29fb89f6c0fa36686b16ef0c71183
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-07 03:04:28 +0430

    HW16: added findByNationalCode() and findBankAccount() to StudentRepository

--- HW16/src/main/java/repository/StudentRepository.java
+++ HW16/src/main/java/repository/StudentRepository.java
@@ -1,9 +1,13 @@
 package repository;
 
 import basics.repository.BaseRepository;
+import entity.BankAccount;
 import entity.Student;
+import entity.enums.AcademicGrade;
 import exceptions.NotFoundException;
 
 public interface StudentRepository extends BaseRepository<Student> {
     Student checkUsernameAndPassword(String username, String password) throws NotFoundException;
+    Student findByNationalCode (String nationalCode);
+    BankAccount findBankAccount (Student student);
 }
--- HW16/src/main/java/repository/impl/StudentRepositoryImpl.java
+++ HW16/src/main/java/repository/impl/StudentRepositoryImpl.java
@@ -1,7 +1,9 @@
 package repository.impl;
 
 import basics.repository.impl.BaseRepositoryImpl;
+import entity.BankAccount;
 import entity.Student;
+import entity.enums.AcademicGrade;
 import exceptions.NotFoundException;
 import repository.StudentRepository;
 import utility.Constants;
@@ -21,4 +23,22 @@ public class StudentRepositoryImpl extends BaseRepositoryImpl<Student> implement
         query.setParameter("p", password);
         return (Student) query.getSingleResult();
     }
+
+    @Override
+    public Student findByNationalCode(String nationalCode) {
+        String hql = "select s from Student s where s.nationalCode =:n ";
+        Query query = entityManager.createQuery(hql, Student.class);
+        query.setParameter("n",nationalCode);
+        return (Student) query.getSingleResult();
+    }
+
+    @Override
+    public BankAccount findBankAccount(Student student) {
+        String hql = "select s.bankAccount from Student s where s.id =:i ";
+        Query query = entityManager.createQuery(hql);
+        query.setParameter("i",student.getId());
+        return (BankAccount) query.getSingleResult();
+    }
+
+
 }
