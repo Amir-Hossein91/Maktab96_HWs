08d5ff03f8ed9de6cfb54bababe7761451b9d3f5
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-07 03:03:00 +0430

    HW16: injected the Availability period of each LoanType to it as a property

--- HW16/src/main/java/entity/enums/LoanType.java
+++ HW16/src/main/java/entity/enums/LoanType.java
@@ -1,7 +1,22 @@
 package entity.enums;
 
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 public enum LoanType {
-    EDUCATIONAL,
-    HOUSE_RENT,
-    TUITION
+    EDUCATIONAL(Availability.PER_COURSE),
+    HOUSE_RENT(Availability.PER_GRADE),
+    TUITION(Availability.PER_GRADE);
+
+    @Enumerated(value = EnumType.STRING)
+    private final Availability availability;
+
+    LoanType (Availability availability){
+        this.availability = availability;
+    }
+
+    public Availability getAvailability(){
+        return this.availability;
+    }
 }
