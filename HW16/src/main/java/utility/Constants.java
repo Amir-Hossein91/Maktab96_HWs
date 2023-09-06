dec8d0a8d8d4b46c22c3930ae7d60a952988b57f
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-07 02:57:41 +0430

    HW16: altered Constants. added loan registration opening dates

--- HW16/src/main/java/utility/Constants.java
+++ HW16/src/main/java/utility/Constants.java
@@ -1,5 +1,8 @@
 package utility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class Constants {
@@ -23,13 +26,21 @@ public class Constants {
     public static final String INCORRECT_USERNAME_OR_PASSWORD = "Username or password is incorrect!";
     public static final String LOAN_REGISTER_TIME_OUT = "Can't register for a loan after graduation year!";
     public static final String REPAY_TIME_OUT = "Can't repay a loan until after graduation!";
+    public static final String NOT_IN_REGITRATION_PERIOD = "Registration is not active now!";
 
     public static final Character[] UPPER_CASE_CHARACTERS = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     public static final Character[] LOWER_CASE_CHARACTERS = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     public static final Character[] SPECIAL_CHARACTERS = {'&','%','$','#','@'};
     public static final Character[] DIGIT_CHARACTERS = {'0','1','2','3','4','5','6','7','8','9'};
 
+    public static final int oddCoursesRegisterMonth = 9;
+    public static final List<Integer> oddCourseRegisterDays = new ArrayList<>(Arrays.asList(23,24,25,26,27,28,29));
+    public static final int evenCourseRegisterMonth = 1;
+    public static final List<Integer> evenCourseRegisterDays = new ArrayList<>(Arrays.asList(14,15,16,17,18,19,20));
+
+
 
     public static final String[] REGISTRY_MENU = {"LOGIN/REGISTER MENU","Register","Login","Exit"};
     public static final String[] STUDENT_LOAN_MENU = {"STUDENT LOAN MENU","Loan Register","loan Repay","Logout"};
+    public static final String[] LOAN_REGISTER_MENU = {"LOAN REGISTER MENU","See Available loans","Register for a loan","Back"};
 }
