d425ee78e9243d21c50bd9c34779666f6d34a2a6
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-09-11 03:04:02 +0430

    HW16: wrote some tests for loanServiceImpl

---
+++ HW16/src/test/java/service/impl/LoanServiceImplTest.java
@@ -0,0 +1,35 @@
+package service.impl;
+
+import entity.BankAccount;
+import entity.Loan;
+import entity.Student;
+import entity.enums.LoanType;
+import org.junit.jupiter.api.Test;
+import utility.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanServiceImplTest {
+
+    LoanServiceImpl loanService = ApplicationContext.loanService;
+
+    @Test
+    void savingLoan(){
+        Student student = Student.builder().id(1).build();
+        Loan loan = Loan.builder().amount(2000000L).borrower(student).registrationDate(ApplicationContext.currentDate).build();
+        assertNotNull(loanService.saveOrUpdate(loan));
+    }
+
+    @Test
+    void choosingALoanSetsARegistrationDateForIt(){
+        Loan loan = Loan.builder().loanType(LoanType.TUITION).build();
+        assertNotNull(loanService.chooseLoan(loan).getRegistrationDate());
+    }
+
+    @Test
+    void possibleLoansIfNotTakenWillBeInPossibleLoansList(){
+        Student student = Student.builder().id(2).build();
+        assertFalse(loanService.getPossibleLoans(student).isEmpty());
+    }
+
+}
